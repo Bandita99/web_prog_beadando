0c408e3753e48e144e3cdf420522ea4f3b64b34f
Bandita99 <119358402+Bandita99@users.noreply.github.com>
2022-11-29 23:03:18 +0100

    Add files via upload

---
+++ adomany.tpl.php
@@ -0,0 +1,21 @@
+
+<div id="adomany">
+<p>Örömmel fogadunk minen adományt</p>
+<p>Az adományok teljes egészét négylábúinkra költjük hiszen lakhatásuk és élelmezésük számok növekedésével egyre nagyobb összeget emészt fel.</p>
+<h3>Amit bármikor szívesen fogadunk</h3>
+<ul>
+<li>Száraz táp</li>
+<li>Nedves táp</li>
+<li>Anyagi felajánlások</li>
+<li>Alkalmi kutyasétáltatás</li>
+<li>Állatorvosi gyógyszerek</li>
+<li>Állatorvosi vizsgálatok</li>
+<li>Önkéntes munka</li>
+<li>40 óra közösségi szolgálat diákok részére</li>
+<li>használt irodai eszközök</li>
+<li>Munka felajánlások pl barkácsolás</li>
+<li>Névleges örökbefogadás</li>
+</ul> 
+<h3>Nagyon köszönjük, hogy gondolsz ránk.</h3>
+<h3>Kérjük támogassa adója 1%-ával az Eb Árvaház Nonprofit Egyesületet! Adószámunk: 18524105-1-41</h3> 
+</div>
