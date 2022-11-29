ec0d2eb02540774375d587644ab8833e740c8457
ras2tt <118926859+ras2tt@users.noreply.github.com>
2022-11-29 23:14:42 +0100

    Update cimlap.tpl.php

--- cimlap.tpl.php
+++ cimlap.tpl.php
@@ -1,1 +1,36 @@
 
+<div id="cimlap">
+<h1> 
+Kedves Sétáltatók, Látogatók és leendő Gazdik!
+</h1>
+<p>
+Menhelyünkön a sétáltatás és a kutyusok szeretgetése 09:00 és 14:30 óra között lehetséges, ugyanekkor van lehetőség ismerkedésre is védenceinkkel.
+Kis árváink egytől egyig imádják a plusz testmozgást és foglalkozást.
+A hét minden napján várjuk az érdeklődőket, de kérjük, mindenki gondoskodjon váltóruháról, stabil cipőről, mert bizony ez egy ilyen játék 
+</p>
+<p>
+Kérjük továbbá a kedves sétáltatókat, hogy tartsák be a gondozók által elmondott szabályokat és mindenki a kijelölt sétáltató útvonalat használja, hogy ne zavarjuk Tárnok lakóit! Legyünk tekintettel egymásra!  Köszönjük!
+Ha valaki szeretne részt venni ezeken napokon, kérjük e-mail vagy Messenger üzenet formájában jelezze felénk. Aki nem regisztrál a megadott eseményünkre, sajnos nem tudjuk fogadni.
+Köszönjük a megértést!
+</p>
+<p>
+Kérjük továbbá a kedves sétáltatókat, hogy tartsák be a gondozók által elmondott szabályokat és mindenki a kijelölt sétáltató útvonalat használja, hogy ne zavarjuk Tárnok lakóit! Legyünk tekintettel egymásra!  Köszönjük!
+Ha valaki szeretne részt venni ezeken napokon, kérjük e-mail vagy Messenger üzenet formájában jelezze felénk. Aki nem regisztrál a megadott eseményünkre, sajnos nem tudjuk fogadni.
+Köszönjük a megértést!
+</p>
+
+</div>
+<center>
+<iframe width="600" height="350" src="https://www.youtube.com/embed/fdqMf9F4vvk" title="YouTube video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+<br>
+<br>
+ 
+<video width="560" height="315" controls>
+<source src="./videos/video1.mp4" type="video/mp4">
+
+</video>
+
+
+
+</div>
