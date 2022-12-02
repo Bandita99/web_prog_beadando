f77998959f75821cf85a276bdf12eec4eac096fc
Bandita99 <119358402+Bandita99@users.noreply.github.com>
2022-12-02 22:30:08 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
-﻿<?php
-include('./include/config.inc.php');
+<?php
+include('./includes/config.inc.php');
 
 $keres = current($oldalak);
 if (isset($_GET['oldal'])) {
-	if (isset($oldalak[$_GET['oldal']]) && file_exists("./template/oldalak/{$oldalak[$_GET['oldal']]['fajl']}.tpl.php")) {
+	if (isset($oldalak[$_GET['oldal']]) && file_exists("./templates/pages/{$oldalak[$_GET['oldal']]['fajl']}.tpl.php")) {
 		$keres = $oldalak[$_GET['oldal']];
 	}
 	else { 
@@ -12,5 +12,5 @@ if (isset($_GET['oldal'])) {
 	}
 }
 
-include('./template/index.tpl.php'); 
+include('./templates/index.tpl.php'); 
 ?>
